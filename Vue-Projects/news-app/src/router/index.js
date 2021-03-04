9cfc72ed9a5202bb6aab5139f1c950d964743ba7
justacoder5 <justacoder5@outlook.com>
2021-03-04 18:45:39 +0000

    Update

--- Vue-Projects/news-app/src/router/index.js
+++ Vue-Projects/news-app/src/router/index.js
@@ -4,6 +4,12 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
+const ORIGINAL_PUSH = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push(location) {
+  return ORIGINAL_PUSH.call(this, location).catch(err=>err)
+}
+
 const routes = [
   {
     path: '/',
@@ -17,6 +23,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact.vue')
   }
 ]
 
